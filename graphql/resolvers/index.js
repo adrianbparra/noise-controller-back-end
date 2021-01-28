eb5da84c9e98e0a19f383e90c3e54ab65b399547
adrianbparra <adrianplane12@gmail.com>
2021-01-27 18:46:58 -0700

    classes query
    Add teacher query to display all classes.

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,29 @@
+const { ApolloError } = require("apollo-server");
+
 const classResolvers = require("./classes");
 const userResolvers = require("./users");
 const scoresResolvers = require("./scores");
+
+const Class = require("../../models/Class")
+
 
 module.exports = {
     Class: {
         highestScore(parent){
             return Math.max(0, ...parent.scores.map(s => s.score))
+        }
+    },
+    User: {
+
+        async classes(parent){
+            try {
+
+                const classes = await Class.find({teacherId: parent._id}) 
+                return classes  
+                
+            } catch (error) {
+                ApolloError("Unable to get User classes", {errors : error})
+            }
         }
     },
     Query: {
